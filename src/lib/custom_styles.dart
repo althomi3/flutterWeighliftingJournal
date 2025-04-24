55b53a4fe6b9e5f71ca6c9ffc13d6410c4b65017
althomi3 <alischa.thomas@aalto.fi>
2025-04-24 13:18:36 +0200

    adds global styling file, customized ListView widget for different breakpoints

---
+++ src/lib/custom_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Style{
+static final ButtonStyle secondaryButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: Color.fromARGB(255, 248, 223, 255)
+);
+}
--- src/lib/widgets/form_list.dart
+++ src/lib/widgets/form_list.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'form.dart';
 import 'package:get/get.dart';
-
-
+import '../custom_styles.dart';
 
 class FormListWidgetSmall extends StatelessWidget {
   final formController = Get.find<FormController>();
@@ -12,8 +11,8 @@ class FormListWidgetSmall extends StatelessWidget {
     return Obx(
       () => formController.size == 0
           ? Text('No sets')
-          : Expanded(
-            child: ListView(
+          : 
+            ListView(
               scrollDirection: Axis.vertical,
                 children: formController.liftingsets
                     .map(
@@ -23,6 +22,7 @@ class FormListWidgetSmall extends StatelessWidget {
                             ListTile(
                               title: Text('${liftingset.exercise}'),
                               subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start, // aligns the tile content to the left
                               children: [
                                 Text('Set: ${liftingset.liftingset}'),
                                 Text('Reps: ${liftingset.reps}'),
@@ -31,11 +31,14 @@ class FormListWidgetSmall extends StatelessWidget {
                                 ],
                               ),
                             ),
-                            Row(
+                            Row( // aligns buttons horizontally
+                              mainAxisAlignment: MainAxisAlignment.start, // aligns button to right of card
                               children: [
                                 TextButton(onPressed: (){}, 
+                                style: Style.secondaryButtonStyle,
                                 child: Text('Edit')),
                                 TextButton(onPressed: (){}, 
+                                style: Style.secondaryButtonStyle,
                                 child: Text('Delete'))
                             ],
                             ),
@@ -45,7 +48,135 @@ class FormListWidgetSmall extends StatelessWidget {
                         ).toList(),
                     ),
               
-      ),
+    
+    );
+  }
+}
+
+class FormListWidgetMedium extends StatelessWidget {
+  final formController = Get.find<FormController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => formController.size == 0
+          ? Text('No sets')
+          : ListView(
+              scrollDirection: Axis.vertical,
+                children: formController.liftingsets
+                    .map(
+                      (liftingset) => Card(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end, // aligned to end so that buttons end on same line as notes
+                          children: [
+                            Expanded(
+                              flex: 7,
+                              child: ListTile(
+                              title: Text('${liftingset.exercise}'),
+                              subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start, // aligns the tile content to the left
+                              children: [
+                                Text('Set: ${liftingset.liftingset}'),
+                                Text('Reps: ${liftingset.reps}'),
+                                Text('Weight (kg): ${liftingset.weight}'),
+                                Text('Personal notes: ${liftingset.notes}'),
+                                ],
+                              ),
+                            ),
+                            ),
+                            Expanded(
+                              flex: 3,
+                              child: Align(
+                                alignment: Alignment.topRight,
+                                child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: ElevatedButton(
+                                          onPressed: (){}, 
+                                          style: Style.secondaryButtonStyle,
+                                          child: Text('Medium Edit'),
+                                      ), 
+                                    ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: ElevatedButton(
+                                          onPressed: (){}, 
+                                          style: Style.secondaryButtonStyle,
+                                          child: Text('Delete'),
+                                      ),
+                                      ),
+                                    ],
+                                    
+                                  ),
+                                  
+                                  
+                              ],
+                              ),
+                            ),
+                            )
+                            
+                            
+                          ],
+                          ),
+                          ),
+                        ).toList(),
+                    ),
+    );
+  }
+}
+
+class FormListWidgetLarge extends StatelessWidget {
+  final formController = Get.find<FormController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => formController.size == 0
+          ? Text('No sets')
+          : ListView(
+              scrollDirection: Axis.vertical,
+                children: formController.liftingsets
+                    .map(
+                      (liftingset) => Card(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end, // aligned to end so that buttons end on same line as notes
+                          children: [
+                            Expanded(
+                              child: ListTile(
+                              title: Text('${liftingset.exercise}'),
+                              subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start, // aligns the tile content to the left
+                              children: [
+                                Text('Set: ${liftingset.liftingset}'),
+                                Text('Reps: ${liftingset.reps}'),
+                                Text('Weight (kg): ${liftingset.weight}'),
+                                Text('Personal notes: ${liftingset.notes}'),
+                                ],
+                              ),
+                            ),
+                            ),
+                            Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  TextButton(onPressed: (){}, 
+                                  style: Style.secondaryButtonStyle,
+                                  child: Text('Edit')),
+                                  TextButton(onPressed: (){}, 
+                                  style: Style.secondaryButtonStyle,
+                                  child: Text('Delete'))
+                              ],
+                            ),
+                            
+                          ],
+                          ),
+                          ),
+                        ).toList(),
+                    ),
     );
   }
 }
